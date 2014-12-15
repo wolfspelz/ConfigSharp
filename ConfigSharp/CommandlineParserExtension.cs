88970131cb627770e5b11c86f8611beb6585997b
Heiner Wolf <wolf.heiner@gmail.com>
2014-12-15 13:24:26 +0100

    Unittest for ParseCommandline

--- ConfigSharp/CommandlineParserExtension.cs
+++ ConfigSharp/CommandlineParserExtension.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ConfigSharp
 {
-    internal static class CommandlineParserExtension
+    public static class CommandlineParserExtension
     {
         public static List<string> ParseCommandline(this string s)
         {
